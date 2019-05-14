4208a39143dd369b1f3f01b07d15b1f6ecf63b19
danaahm <danaahmadi2014@gmail.com>
2019-05-15 02:35:39 +0430

    INITIAL COMMIT

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '42d62eb7d5291eeba627863ca158d84b6cf63b32';
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
