ed7c88026bcca090c2b717ea9b98f00c7d3e590a
nclelw <nicole_low93@hotmail.com>
2019-05-15 13:55:24 +0430

    Added routes to be able to test templates

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,32 @@ from app import app
 def index():
     user = {'username': 'MiguwSel'}
     return render_template('index.html', title='Home', user=user)
+
+@app.route('/submit')
+def submit():
+	return render_template("submit_video.html")
+
+@app.route('/view')
+def view():
+	return render_template("view_video.html")
+
+@app.route('/login')
+def login():
+	return render_template("login.html")
+
+@app.route('/dash')
+def dash():
+	return render_template("dashboard.html")
+
+@app.route('/users')
+def users():
+	return render_template("users.html")
+
+
+@app.route('/votes')
+def votes():
+	return render_template("votes.html")
+
+@app.route('/videos')
+def videos():
+	return render_template("videos.html")
