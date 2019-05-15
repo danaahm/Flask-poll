90a57fb0cc6f33fc5ff14950c7373f1081bad211
nclelw <nicole_low93@hotmail.com>
2019-05-16 01:39:48 +0430

    Login for users and admins is done with neccessary changes in htmls

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
+from flask_login import LoginManager
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -12,5 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,52 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
-
+from app.forms import LoginForm
+from app.forms import RegisterForm
+from flask_login import current_user, login_user,logout_user
+from app.models import User
+from app import db
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'MiguwSel'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home')
+
+@app.route('/register',methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	form = RegisterForm()
+	if form.validate_on_submit():
+		user = User(email=form.email.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash('Congratulations, you are now a registered user!')
+		return redirect(url_for('login'))
+	return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(email=form.email.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid username or password')
+			return redirect(url_for('login'))
+		login_user(user, True)
+		if user.is_admin:
+			return redirect(url_for('dashboard'))
+		else:
+			return redirect(url_for('index'))
+
+	return render_template('login.html', title='Log in', form=form)
+
+@app.route("/logout")
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
 
 @app.route('/submit')
 def submit():
@@ -16,13 +56,11 @@ def submit():
 def view():
 	return render_template("view_video.html")
 
-@app.route('/login')
-def login():
-	return render_template("login.html")
-
-@app.route('/dash')
-def dash():
+@app.route('/dashboard')
+def dashboard():
 	return render_template("dashboard.html")
+
+
 
 @app.route('/users')
 def users():
